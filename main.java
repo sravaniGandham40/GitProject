5bb0a62c8424afadd98561ae758a7b25ab2a91e3
dev1 <dev1@gmail.com>
2022-07-20 18:47:57 +0530

    second commit for signup

--- main.java
+++ main.java
@@ -5,4 +5,7 @@ Class A{
 public void add(int value1, int value2){
 System.out.println("The result is" +(value1+value2);
 }
+public void substact(int v1, int v2){
+System.out.println("The result is"+(v1*v2)
+}
 }
