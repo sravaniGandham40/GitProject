85f0b1c1a8dad6143a2dcc0006f8b0353071f3ce
dev1 <dev1@gmail.com>
2022-07-20 09:25:25 +0530

    dev1 first commit for add()

--- main.java
+++ main.java
@@ -2,4 +2,7 @@ Class A{
  public static void main(String args[]){
     System.out.println("Hello class");
 }
+public void add(int value1, int value2){
+System.out.println("The result is" +(value1+value2);
+}
 }
